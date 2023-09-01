d0744c790dfc74ad4d314e97ea1fb1bb488cf9a9
Vitor Pereira <vitorpereiraresende26@gmail.com>
2023-08-31 21:42:13 -0300

    axios done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AuthContentComponent } from './auth-content/auth-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AuthContentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/auth-content/auth-content.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { AxiosService } from '../axios.service';
+
+@Component({
+  selector: 'app-auth-content',
+  templateUrl: './auth-content.component.html',
+  styleUrls: ['./auth-content.component.css']
+})
+export class AuthContentComponent {
+  data: string[] = [];
+
+  constructor(private axiosService: AxiosService){}
+
+  ngOnInit(): void{
+    this.axiosService.request(
+      "GET",
+      "/messages",
+      {}
+    ).then(
+      (response) => this.data = response.data
+    );
+  }
+}
